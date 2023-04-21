3aaab9389382eaeb1ccdce838a8410dbe1657883
DIVVELA-MANIKANTA <manikanta.divvela124@gmail.com>
2023-04-21 19:49:05 +0530

    commit

--- p.py
+++ p.py
@@ -1,26 +1,32 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('Sparknew').getOrCreate()
-# print("spark: ", spark)
+print("spark: ", spark)
 
-# #Creates Empty RDD
-# emptyRDD = spark.sparkContext.emptyRDD()
-# print(emptyRDD)
+#Creates Empty RDD
+emptyRDD = spark.sparkContext.emptyRDD()
+print(emptyRDD)
 
-# #Create Schema
-# from pyspark.sql.types import StructType,StructField, StringType
-# schema = StructType([
-#   StructField('firstname', StringType(), True),
-#   StructField('middlename', StringType(), True),
-#   StructField('lastname', StringType(), True)
-#   ])
-# #Create empty DataFrame from empty RDD
-# df = spark.createDataFrame(emptyRDD,schema)
-# df.printSchema()
+#Create Schema
+from pyspark.sql.types import StructType,StructField, StringType
+schema = StructType([
+  StructField('firstname', StringType(), True),
+  StructField('middlename', StringType(), True),
+  StructField('lastname', StringType(), True)
+  ])
+#Create empty DataFrame from empty RDD
+df = spark.createDataFrame(emptyRDD,schema)
+df.printSchema()
 
 
 df = spark.read.csv(r"Sample-Spreadsheet-100-rows.csv")
+print(df.collect())
 print(df.show(truncate= False))
 print("pythondf-------->")
 new =df.toPandas()
-print(new)
+print(new['_c1'].astype("str"))
+print(df.select('_c1').show())
+new = df.select(df['_c1'],df['_c2'])
+print(new.show())
+print(new.collect())
+
